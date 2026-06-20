beda3b5276709dfe665f895ae73cdac3eb2467c4
yuyay-cloud <yuyay0325@gmail.com>
2026-06-20 18:37:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const APP_VERSION = '2.24';
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+const APP_VERSION = '2.24-dark-splash-v2';
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
