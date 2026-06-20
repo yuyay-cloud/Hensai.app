357a2833d07363ecc0776499e5a5fac466f198b5
yuyay-cloud <yuyay0325@gmail.com>
2026-06-20 10:34:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-// cleanup-only service worker. It removes older caches and unregisters itself.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-      .then(() => self.registration.unregister())
-  );
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
