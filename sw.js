87056363c08e921f2e401d0eeb8ce6ad237b53cb
yuyay-cloud <yuyay0325@gmail.com>
2026-06-21 21:10:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,7 @@
-const CACHE_NAME = "hensai-v2.30";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192-v230.png",
-  "./icon-512-v230.png",
-  "./icon-192-maskable-v230.png",
-  "./icon-512-maskable-v230.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if(event.request.method !== "GET") return;
-  if(event.request.mode === "navigate"){
-    event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
-    return;
-  }
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
