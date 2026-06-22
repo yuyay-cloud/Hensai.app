9bb1222473933099e0e5a95eb98b59ea28804154
yuyay-cloud <yuyay0325@gmail.com>
2026-06-22 21:15:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hensai-v2361-shell';
+const CACHE = 'hensai-v2361-hero-splash-r1';
 const ASSETS = [
   './', './index.html', './manifest.json',
   './icon-192-v2361.png', './icon-512-v2361.png',
