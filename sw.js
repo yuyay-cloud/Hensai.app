e6b6783d81759c65aa44254d0de72b944295b12b
yuyay-cloud <yuyay0325@gmail.com>
2026-06-22 19:43:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,19 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+const CACHE = 'hensai-v236-shell';
+const ASSETS = [
+  './', './index.html', './manifest.json',
+  './icon-192-v236.png', './icon-512-v236.png',
+  './icon-192-maskable-v236.png', './icon-512-maskable-v236.png',
+  './proposal_logo_light_transparent.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => response).catch(() => caches.match(event.request)));
 });
