a43390f47a0be1c8781b48abc0c1765bae4dd7df
yuyay-cloud <yuyay0325@gmail.com>
2026-06-22 21:58:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hensai-v2361-schedule-light-r5';
+const CACHE = 'hensai-v2361-input-manual-r6';
 const ASSETS = [
   './', './index.html', './manifest.json',
   './icon-192-v2361.png', './icon-512-v2361.png',
